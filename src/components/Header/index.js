ce94c8d36b03167285fbd92d20b51dcd6ef873ec
Kasra Zafari <zafari.kasra@gmail.com>
2025-02-20 09:18:34 +0330

    pages/about/company

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,6 +8,7 @@ const Header = () => {
                 <nav>
                     <Link href="/">Home</Link>
                     <Link href="/about">About</Link>
+                    <Link href="/about/company">Company</Link>
                     <Link href="/products">Products</Link>
                 </nav>
             </header>
