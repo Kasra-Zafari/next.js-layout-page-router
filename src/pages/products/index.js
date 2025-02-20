ebb57808da9d033d1cbe6ccb2c0c10e15158c418
Kasra Zafari <zafari.kasra@gmail.com>
2025-02-20 09:28:37 +0330

    products fetch

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,14 +1,29 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
-const ProductsPage = ()=>{
+const ProductsPage = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((data) => {
+                setProducts(data);
+            })
+    }, [])
+
+    console.log(products);
+    
+
     return (
         <>
-        <Head>
-            <title>
-                Products Page
-            </title>
-        </Head>
-        <h1>Products</h1>
+            <Head>
+                <title>
+                    Products Page
+                </title>
+            </Head>
+            <h1>Products</h1>
         </>
     )
 }
